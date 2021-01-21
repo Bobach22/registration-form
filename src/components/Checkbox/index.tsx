998c2edca39686eae94bda8b995601b43a2e7773
Bobach22 <l.user2200@gmail.com>
2021-01-21 14:54:47 +0500

    exposed input element of custom checkbox

--- src/components/Checkbox/index.tsx
+++ src/components/Checkbox/index.tsx
@@ -1,24 +1,38 @@
 import styles from "./style.module.scss";
 import {Checkmark} from "../../assets/icons/Checkmark";
+import  React, { MouseEvent } from "react";
 
 
 type Props={
     checked:boolean;
-    onClick:()=>React.MouseEventHandler<HTMLInputElement>|any;
-}
+}&React.HtmlHTMLAttributes<HTMLElement>
 
 export const Checkbox: React.FC<Props> = ({
   children,
   onClick,
   checked=false
 }) => {
+
+  const ref=React.useRef<any>();
+
+const handleClick=()=>{
+   if(ref.current){
+     ref.current.click();
+   }
+}
+
+const handleInputClick=(e:MouseEvent<HTMLInputElement>)=>{
+  if(onClick){
+    onClick(e);
+  }
+}
   return (
     <div className={styles.wrapper}>
-      <input type="checkbox" className={styles.checkbox__hidden}
+      <input onClick={handleInputClick}  ref={ref} type="checkbox" className={styles.checkbox__hidden}
         aria-checked={checked}
       />
-      <span role="checkbox" aria-checked={checked}
-        onClick={onClick}
+      <span role="checkbox" onClick={handleClick}  aria-checked={checked}
+        
         className={`${styles.checkbox} ${checked?styles.checked:styles.unchecked}`}>{checked&&<Checkmark />}</span>
       <label className={styles.label}>{children}</label>
     </div>
